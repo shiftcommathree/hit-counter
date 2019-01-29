ed9883333fc0df0b5ccdb5fb7dc02ba69c44a7fe
ShiftCommaThree <31151788+shiftcommathree@users.noreply.github.com>
2019-01-29 12:09:28 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# hit_counter
+
+A basic Python web app to demonstrate linking docker containers
+
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return 'Hello World! I have been seen %i times.\n' % int(redis.get('hits'))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
